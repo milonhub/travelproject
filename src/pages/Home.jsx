a45209f3f41236e1a48514cdc1ac206f5367391c
milonhub <milonru150@gmail.com>
2023-11-21 00:18:03 -0800

    up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,11 +43,11 @@ const Home = () => {
               <p className="text-sm text-white">Find Awesome hotel, tour, car and activities in London</p>
             </div>
 
-            <img src="https://source.unsplash.com/N981UfUhF7U/300x280" className="h-[250px] w-[180px] mx-4" />
+            <img src="https://source.unsplash.com/N981UfUhF7U/300x280" className="h-[250px] w-[180px] mx-4 rounded-lg  transition duration-300 ease-in-out hover:scale-110 hover:shadow-lg dark:hover:shadow-black/30" />
           </div>
 
         </div>
-        <img src="https://source.unsplash.com/7etIYqqw2jU/300x500" alt="" className="h-[450px] w-[300px] p-9" />
+        <img src="https://source.unsplash.com/7etIYqqw2jU/300x500" alt="" className="h-[380px] w-[300px] rounded-3xl p-4  transition duration-300 ease-in-out hover:scale-110" />
 
       </div>
         <div>
@@ -60,7 +60,11 @@ const Home = () => {
       <div>
         <div className="hero min-h-screen bg-base-200">
           <div className="hero-content flex-col lg:flex-row gap-32">
-            <img src="https://source.unsplash.com/1zFlrpWTs3Y/300x280" className="w-[350px] h-[380px] shadow-2xl" />
+            <div className="relative w-[350px] h-[380px] overflow-hidden bg-cover bg-no-repeat rounded-xl">
+              <img src="https://source.unsplash.com/1zFlrpWTs3Y/300x280" className="w-[350px] h-[380px] rounded-xl" />
+              <div
+                className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-0 transition duration-300 ease-in-out hover:opacity-40"></div>
+            </div>
             <div>
               <h1 className="text-4xl italic text-blue-600">Why Choose Us </h1>
               <h1 className="text-4xl font-bold">Plan Your Trip <br /> with Trisog</h1>
@@ -155,7 +159,7 @@ const Home = () => {
 
               <div key={id}>
                 <div className="hero-content flex-col lg:flex-row">
-                  <img src={url} className="w-32 h-32" />
+                  <img src={url} className="w-32 h-32 rounded-xl" />
                   <div className="w-64">
                     <p className="text-xl flex">{date} <li className="mx-6">Admin</li></p>
                     <p className="text-xl font-semibold">{des}</p>
@@ -170,6 +174,16 @@ const Home = () => {
 
       <hr className="my-8 mx-[10%]"></hr>
 
+      <div className="hero min-h-[600px]" style={{ backgroundImage: 'url(https://source.unsplash.com/cWHAW_tjk7Q)' }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div>
+            <h1 className="text-xl text-yellow-500 italic">Are you ready to travel?</h1>
+            <p className="text-">Tevily is world Leading Online Tour Booking Platform</p>
+          </div>
+        </div>
+      </div>
+
       <div className="flex gap-5 justify-center mb-8 -z-10 hover:z-10">
         <div className="flex flex-col justify-center gap-6">
           <p className="rotate-[-90deg] text-xl font-semibold">@Trisog</p>
@@ -181,7 +195,7 @@ const Home = () => {
             const { url, id } = image;
             return (
               <article key={id}>
-                <img src={url} alt="pic" className="w-[280px] h-[300px]" />
+                <img src={url} alt="pic" className="w-[280px] h-[300px] transition duration-300 ease-in-out hover:scale-110" />
               </article>
             )
           })
