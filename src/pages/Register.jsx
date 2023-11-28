118b8fe6a9418ee8d510f21c1afbb78da2085cf9
milonhub <milonru150@gmail.com>
2023-11-29 00:43:06 +0600

    update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,57 +1,109 @@
 import { useState } from 'react';
-import {auth} from '../firebase/firebaseConfig';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebaseConfig';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+
+} from 'firebase/auth';
 
 const Register = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginpassword, SetLoginPassword] = useState('');
   // const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({})
 
-  const handleSignin =async () => {
-   try {
-    const user = await createUserWithEmailAndPassword(auth, username, password);
-    console.log(user)
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  })
 
-   } catch(error){
+  const handleSignin = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, username, password);
+      console.log(user)
+
+    } catch (error) {
       console.log(error.message)
-   }
+    }
+  }
+  const handleSignOut = async () => {
+    await signOut(auth)
   }
 
+  const handlelogin = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, loginEmail, loginpassword);
+      console.log(user)
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   return (
     <div className='flex justify-center w-full h-[500px] my-12'>
 
       <div className='w-[600px] h-[500px] bg-slate-400 p-12 rounded-xl'>
-        
-          <div>
-            <h2 className='text-4xl'>Sign-in Page</h2>
-            <form>
-              <div className='mt-8'>
-                <label className='text-2xl'>
-                  Username:
-                  <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </label>
-              </div>
-              <br />
+
+        <div>
+          <h2 className='text-4xl'>Sign-in Page</h2>
+          <form>
+            <div className='mt-8'>
               <label className='text-2xl'>
-                Password:
+                Username:
                 <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </label>
-              <br />
-              <button type="button" onClick={handleSignin} className='text-2xl my-3 bg-slate-700 px-4 py-2 text-white hover:bg-red-400 rounded-lg'>
-                Register
-              </button>
-            </form>
-          </div>
-        
+            </div>
+            <br />
+            <label className='text-2xl'>
+              Password:
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="button" onClick={handleSignin} className='text-2xl my-3 bg-slate-700 px-4 py-2 text-white hover:bg-red-400 rounded-lg'>
+              Register
+            </button>
+          </form>
+
+         
+        </div>
+
+
+        <div>
+
+          <form>
+            <label>Eamil</label>
+            <input type="email" value={loginEmail} onChange={(e) => {
+              setLoginEmail(e.target.value);
+
+            }} />
+            <label>Password</label>
+            <input type="password" value={loginpassword} onChange={(e) => {
+              SetLoginPassword(e.target.value);
+
+            }} />
+
+            <button type="button" onClick={handlelogin} className='text-2xl my-3 bg-slate-700 px-4 py-2 text-white hover:bg-red-400 rounded-lg'>
+              Log-in
+            </button>
+          </form>
+
+        </div>
+
+        <p>{user?.email}</p>
+          <button type="button" onClick={handleSignOut} className='text-2xl my-3 bg-slate-700 px-4 py-2 text-white hover:bg-red-400 rounded-lg'>
+            Sign-Out
+          </button>
       </div>
     </div>
 
